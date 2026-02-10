449ae676f46237574872dd9767431dccbe23bb16
BeluTomas <belutomas2003@gmail.com>
2026-02-10 19:18:11 -0300

    Arreglo error doble turno

--- app/schemas/curso.py
+++ app/schemas/curso.py
@@ -1,10 +1,12 @@
 from enum import Enum
 from sqlmodel import SQLModel, Field
 
+
 class TurnoCurso(str, Enum):
     Manana = "Manana"
     Tarde = "Tarde"
-    DobleTurno = "Doble Turno"
+    DobleTurno = "DobleTurno"  # ✅ igual a la DB (sin espacio)
+
 
 class CursoBase(SQLModel):
     nombre: str = Field(max_length=255)
@@ -12,18 +14,18 @@ class CursoBase(SQLModel):
     division: str = Field(max_length=50)
     turno: TurnoCurso
 
+
 class CursoPublic(CursoBase):
     idCurso: int
     CUE: str
 
+
 class CursoCreate(CursoBase):
     pass
+
 
 class CursoUpdate(SQLModel):
     nombre: str | None = None
     cicloLectivo: str | None = None
     division: str | None = None
     turno: TurnoCurso | None = None
-
-
-
